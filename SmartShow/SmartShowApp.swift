fcd6143d7f6864f91bdc3724d068546a919796bb
andre-yost <andresyost2000@gmail.com>
2023-04-17 23:52:03 -0400

    Initial Commit

---
+++ SmartShow/SmartShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShowApp.swift
+//  SmartShow
+//
+//  Created by Andre Yost on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
